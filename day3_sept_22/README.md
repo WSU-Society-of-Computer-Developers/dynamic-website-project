9557357428b618d518450348230f51e9fd94365e
Zavaar Shah <thatziv@gmail.com>
2022-09-22 23:03:28 -0400

    day 3

---
+++ day3_sept_22/README.md
@@ -0,0 +1,14 @@
+# Day 3
+
+### Recording URL: [here](https://youtu.be/xxyelsQcz58)
+
+## Objectives
+
+* [X] Create and perform dynamic requests
+* [X] Integrate more interesting APIs with the frontend using Javascript DOM
+* [X] Parse response & request data
+
+## Summary
+
+* Began integrating [openweathermap.org](https://openweathermap.org) API for real-time weather data
+* Demonstrated dynamic requests with various URL-shortening APIs
---
+++ day3_sept_22/index.js
@@ -0,0 +1,60 @@
+// API wrapper
+
+const API = {
+    getRandomDog() {
+        var dogButton = document.querySelector("#dogStatus")
+        dogButton.innerHTML = "Getting dog now."
+        fetch("https://random.dog/woof.json").then((response) => {
+            dogButton.innerHTML = "Received dog"
+            return response.json()
+        }).then(function (data) {
+
+            const dog = data.url // link to the video or picture
+            const isVideo = dog.split('.').pop() == "mp4"
+            // <iframe class="embed-responsive-item" src="..."></iframe>
+            document.querySelector("#dog").setAttribute("src", dog)
+            dogButton.innerHTML = "Get a new dog."
+            // TODO: make images & videos scale better on the html canvas
+        })
+    },
+    shortenURL(url) {
+        fetch(`https://short-link-api.vercel.app/?query=${encodeURIComponent(url)}`,
+            { method: 'GET' })
+            .then(response => response.json())
+            .then((data) => {
+                console.log(data)
+            })
+    }
+}
+
+const WeatherAPI = {
+    APIKEY: "<Your API key here>",
+    kelvinToFahrenheit: (k) => ((k - 273.15) * (9/5) + 32 ), // convert to f
+    getCurrentWeather() {
+        navigator.geolocation.getCurrentPosition(
+            ({ coords }) => {
+                const lat = coords.latitude;
+                const lon = coords.longitude;
+                fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${this.APIKEY}&units=imperial`,
+                    { method: 'GET' })
+                    .then(response => response.json())
+                    .then((data) => {
+                        data = { data, ...{ coords: null } }
+                        let city = data.data.name
+                        let forecast = data.data.main
+                        document.querySelector("#weather").innerHTML = `The weather in <b>${city}</b> is currently <i>${forecast.feels_like}°F</i>`
+                        //forecast.feels_like
+                    })
+            }, console.error, {
+            enableHighAccuracy: true,
+            timeout: 5000,
+            maximumAge: 0
+        })
+    }
+}
+
+
+window.onload = function () {
+    API.getRandomDog()
+    WeatherAPI.getCurrentWeather()
+}
